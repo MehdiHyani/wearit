77f14615462afae17846bfd047606e7643e06bcc
BR4INL3SS <hyanimehdi115@gmail.com>
2022-11-11 17:27:52 +0100

    [refactor]: schema refactoring and seeders update

--- backend/src/seeders/index.ts
+++ backend/src/seeders/index.ts
@@ -1,20 +1,105 @@
 import { createUser } from "../services/user.service";
 import prisma from "../utils/db";
 import log from "../utils/logger";
-import { users } from './seedData'
+import { users, stores, products } from './seedData';
+import { faker } from '@faker-js/faker';
+
+async function seedUsers() {
+    try {
+        await prisma.user.deleteMany();
+        await Promise.all(users.map(async (user) => {
+            const { id } = await createUser({
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+                password: user.password,
+            }, 'admin');
+            return id;
+        }))
+        log.info("Seeded users")
+    } catch (error) {
+        log.info("Error seeding users")
+    }
+}
+
+async function seedStores() {
+    try {
+        await prisma.store.deleteMany();
+        await prisma.store.createMany({
+            data: stores,
+        })
+        log.info("Seeded stores")
+    } catch (error) {
+        log.info("Error seeding stores")
+    }
+}
+
+async function seedProducts() {
+    try {
+        await prisma.product.deleteMany();
+        await prisma.product.createMany({
+            data: products,
+        })
+        log.info("Seeded products")
+    } catch (error) {
+        log.info("Error seeding products")
+    }
+}
+
+async function seedAvailabilities() {
+    try {
+        await prisma.availability.deleteMany();
+        const stores = await prisma.store.findMany();
+        const products = await prisma.product.findMany();
+        await Promise.all(stores.map(async (store) => {
+            await Promise.all(products.map(async (product) => {
+                if(parseInt(faker.random.numeric(3)) % 2)
+                    await prisma.availability.create({
+                        data: {
+                            quantityOnHand: parseInt(faker.random.numeric(2)),
+                            storeId: store.id,
+                            productId: product.id,
+                        }
+                    })
+            }))
+        }))
+        log.info("Seeded availabilities")
+    } catch (error) {
+        log.info("Error seeding availabilities")
+    }
+}
+
+async function seedFeedbacks() {
+    try {
+        const users = await prisma.user.findMany();
+        const products = await prisma.product.findMany();
+        await Promise.all(products.map(async (product) => {
+            await Promise.all(users.map(async (user) => {
+                await prisma.feedback.create({
+                    data: {
+                        feedback: faker.lorem.sentence(),
+                        userId: user.id,
+                        productId: product.id,
+                    }
+                })
+            }))
+        }))
+        log.info("Seeded feedbacks")
+    } catch (error) {
+        log.info("Error seeding feedbacks")
+    }
+}
 
 async function main() {
-    await prisma.user.deleteMany();
-    const createdUsers = await Promise.all(users.map(async (user) => {
-        const { id } = await createUser({
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName,
-            password: user.password,
-        });
-        return id;
-    }))
-    log.info(createdUsers.join(" "))
+    try {
+        await seedUsers();
+        await seedStores();
+        await seedProducts();
+        await seedAvailabilities();
+        await seedFeedbacks();
+    } catch (error) {
+        log.error(error)
+    }
 }
 
 main();
--- backend/src/seeders/seedData.ts
+++ backend/src/seeders/seedData.ts
@@ -25,6 +25,53 @@ export const stores = [
         latitude: 33.531737,
         longitutde: -5.122883,
         openingHours: "Mon-Fri 08:00-17:00;Sat 11:00-15:00",
-        phoneNumber: "+212600000000",
-    }
+        phoneNumber: "+212666666666",
+    },
+    {
+        name: "WEARIT Marrakech",
+        latitude: 31.672609702963296,
+        longitutde: -8.020959125136166,
+        openingHours: "Mon-Fri 08:00-17:00;Sat 11:00-15:00",
+        phoneNumber: "+212666666666",
+    },
+    {
+        name: "WEARIT Casablanca",
+        latitude: 33.55920466221561,
+        longitutde: -7.626430345851527,
+        openingHours: "Mon-Fri 08:00-17:00;Sat 11:00-15:00",
+        phoneNumber: "+212666666666",
+    },
+];
+
+export const products = [
+    {
+        name: 'Morocco Home jersey',
+        price: 299.99,
+        imageUrl: 'https://www.footballshirtculture.com/images/2022/morocco-home-22-23-replica-jersey.jpg'
+    },
+    {
+        name: 'Morocco Away jersey',
+        price: 399.99,
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjF2GpL8ddsRKMCWkG5qV97F4t5lBCDSsbBIbbL1bYUJxmoYSQdKu59QARK-JBpmv-z7OpSA1_4q3zjoAhj8jpL2-cPUkzonHlnB8IDPiD-tjTF3fB6_hs2jsdv4Ftq1-ZHSTze5yvqGPuYjckdwVp9Uab0HCi7F3yDbUOHwDIzkCsVaXcxyBsKhMQ66w/s600/all%20puma%202022%20away%20kits%20national%20teams%20%2835%29.jpg'
+    },
+    {
+        name: 'England Home jersey',
+        price: 299.99,
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhd7xqY5u4xHku6dPuANUU779gRLEqKD2byZphsWsIHnrXyZ65q39GW6Mfqe9JDwUGf63BiLIhUs-sDqA3CkDkc37V-kESgFX6B0kotei_OTkiS5-GJujaINzXEAHS1Gv83CQo7-2YaCkzbSHJksJfPRzzYsMacwEetyZDZVYnnrj7AftVOpEYnuGMd/s600/england-2022-world-cup-kit-4.jpg'
+    },
+    {
+        name: 'England Away jersey',
+        price: 399.99,
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjaLHmzHG2cGdLPqstL-aDUfj_3tBLqgSSMdWuISb0NkyL4id-YsxksLkdHWnGW5CqzqTzzGu5AGWNmHXwxBSC5JhtTpqwtLyQYfD0PoPfbTGolAfHG9NssFas42e8zJf6Jf3mYJ8tTbm3bWSU12bLey6PD5mfUUJeZDBhJXMM25UAUUlpulGzMYvCW/s600/england-2022-world-cup-away-kit-1.jpg'
+    },
+    {
+        name: 'Spain Home jersey',
+        price: 299.99,
+        imageUrl: 'https://blogger.googleusercontent.com/img/a/AVvXsEhn-8DnsswLNLliHLUCkb9fhNI2C1zvHXGravelfrlQYFw2OypbC14f2zLd_6LqVemp0d7k1n-MmsLdBYM5fFRpucrzEs2tBAQGDLSacQfMPgxTmxzz3b52ec318zpIoe9C43A-v8ywAFfnHXmzwELqvex9wmA48zBXhbDFNJnce_3UwL21sRTVyoqV_Q=s600'
+    },
+    {
+        name: 'Spain Away jersey',
+        price: 399.99,
+        imageUrl: 'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhIHggdscfuPVB1Z-p-_H4jC8f3oWiaTnWeJqm5n3iqRnXY8-x74bBXohSJtJ2OpfZRX4HbRYAnwseKgUtrFmSYRCU7D2ZxiVsnbLEUfqSRrM-kICYrRJNLNPA6-4zAcGnBawnVXxhARyOwgcxO9zmUxl9JSRG_6tgLZllMY-axgLmOWcC_IAIplZ74/s600/spain-2022-world-cup-kit-6.jpg'
+    },
 ]
