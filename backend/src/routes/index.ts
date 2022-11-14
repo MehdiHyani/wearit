3af45088d991279bc34c27354712dd3fe3b7dd8a
BR4INL3SS <hyanimehdi115@gmail.com>
2022-11-14 17:44:25 +0100

    feat: app serving routes

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,4 +1,6 @@
-import { Router } from 'express';
+import express, { Router } from 'express';
+import path from 'path';
+import app from '../server';
 import authRouter from './auth.routes';
 import userRouter from './user.routes';
 
@@ -7,6 +9,14 @@ const apiRouter = Router();
 apiRouter.get('/', async (req, res) => {
     res.status(200).send('WEARIT API running perfectly')
 });
+
+if(process.env.NODE_ENV === 'production'){
+    // Serving the app
+    apiRouter.use(express.static(path.join(__dirname, 'build')))
+    apiRouter.get('/*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    });
+}
 
 apiRouter.use('/auth', authRouter);
 
