65d9e8e6d5425c5065e9fd830ca45daa6cd78db1
BR4INL3SS <hyanimehdi115@gmail.com>
2022-12-24 21:41:43 +0100

    revert: cors options

--- backend/src/utils/applyGlobalMiddlewares.ts
+++ backend/src/utils/applyGlobalMiddlewares.ts
@@ -1,19 +1,22 @@
 import { Express, json, urlencoded } from 'express';
-import cors from 'cors';
+import cors, { CorsOptions } from 'cors';
 import helmet from 'helmet';
 import morgan from 'morgan';
 import cookies from 'cookie-parser';
 
 function applyGlobalMiddlewares(expressApp: Express) {
+
+    const corsOptions = {
+        origin: process.env.CLIENT_URL,
+        credentials: true,
+    };
+
     expressApp.enable('trust proxy');
     expressApp.set('trust proxy', true);
     expressApp.use(cookies());
     expressApp.use(json());
     expressApp.use(urlencoded({ extended: true }));
-    expressApp.use(cors({
-        origin: process.env.NODE_ENV === 'production' ? process.env.CLIENT_URL : '*',
-        credentials: true,
-    }));
+    expressApp.use(cors(corsOptions));
     expressApp.use(helmet());
     expressApp.use(morgan(':method :url :status - :response-time ms'));
 }
