0a617c07361446a70c56d10664201d57fcf62893
002FOX <s.elarif@aui.ma>
2022-12-24 22:50:37 +0100

    Order eslint

--- frontend/src/pages/Order.tsx
+++ frontend/src/pages/Order.tsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 const Order = () => {
-  return (
-    <div>Order</div>
-  )
-}
-
-export default Order
+    return (
+        <div>Order</div>
+    );
+};
+//
+export default Order;
